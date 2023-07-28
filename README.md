5082f60f2c83cd287a334575663bb8768fb78ef2
ckie <git-525ff67@ckie.dev>
2023-07-29 01:44:44 +0300

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# skascroll
+
+add scroll buttons for pages (WebExtension)
+
+- firefox mobile intended, but others may work.
+- project template: https://github.com/samrum/vite-plugin-web-extension
+- quite bare
+- the interesting file is `src/entries/contentScript/primary/App.svelte`
+
+## how use,, (dev)
+
+```
+npm i
+```
+
+now these in parallel:
+
+```
+npm run watch
+```
+
+```
+web-ext run --target=firefox-android --source-dir dist
+    Applying config file: ./package.json
+    Running web extension from /home/ckie/git/skascroll/dist
+
+    Android devices found:
+    - XXXXXXXXXXX
+
+    UsageError: Select an android device using --android-device=<name>
+
+web-ext run --target=firefox-android --source-dir dist --android-device=XXXXXXXXXXX
+```
+
+u can edit the code now and it should just reload 
+
+## license
+see `LICENSE` file, it's MIT.
---
+++ src/entries/contentScript/primary/App.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+ // import logo from "~/assets/logo.svg";
+ //
+ // const logoImageUrl = new URL(logo, import.meta.url).href;
+ function s(pages: number) {
+   window.scrollBy(0, pages * window.innerHeight);
+ }
+</script>
+
+<div class="overlay">
+  <button class="up" aria-label="Page up" on:click={() => s(1)}></button>
+  <button class="down" aria-label="Page down" on:click={() => s(-1)}></button>
+  🐈
+</div>
+
+<style>
+  .overlay {
+    z-index: 999991312;
+    position: fixed;
+    bottom: 20px;
+    right: 10px;
+    width: 64px;
+    height: 128px;
+    display: flex;
+    flex-direction: column;
+    background-color: #faf;
+    opacity: 0.8;
+
+    text-align: center;
+  }
+
+ .up, .down {
+   width: 100%;
+   height: 64px;
+   font-size: 32px;
+ }
+ .up::before {
+   content: "↑"
+ }
+ .down::before {
+   content: "↓"
+ }
+</style>
